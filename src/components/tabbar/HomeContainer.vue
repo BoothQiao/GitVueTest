0f9900d16d7166129b62df50b33c7fbe625f7815
BoothQiao <278198927@qq.com>
2019-02-28 09:06:00 +0800

    第三次提交2.28

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>HomeContainer</h3>
+  <mt-swipe :auto="4000">
+  <mt-swipe-item>1</mt-swipe-item>
+  <mt-swipe-item>2</mt-swipe-item>
+  <mt-swipe-item>3</mt-swipe-item>
+</mt-swipe>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 // 入口文件
 import Vue from 'vue'
 // 1.1 导入路由的包
@@ -13,7 +12,7 @@ import './css/app.css'
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
 // 导入扩展图标样式
-//import './lib/mui/css/icons-extra.css'
+import './lib/mui/css/icons-extra.css'
 
 // // 导入所有 MUI 
 // import MintUI from 'mint-ui'
@@ -22,11 +21,15 @@ import './lib/mui/css/mui.min.css'
 // //将MintUI安装到Vue中
 // Vue.use(MintUI)
 
-import { Header } from 'mint-ui'
-//Vue.component(Toast.name, Toast)
+import {
+  Header,
+  Swipe,
+  SwipeItem
+} from 'mint-ui'
 Vue.component(Header.name, Header)
-// Vue.use(Toast)
-//Vue.use(Button)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
 
 
 // 1.3 导入自己的 router.js 路由模块
@@ -37,8 +40,7 @@ import router from './router.js'
 import app from './App.vue'
 
 var vm = new Vue({
-    el: '#app',
-    render: c => c(app),
-    router // 1.4 挂载路由对象到 VM 实例上
-  })
-
+  el: '#app',
+  render: c => c(app),
+  router // 1.4 挂载路由对象到 VM 实例上
+})
